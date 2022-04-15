b966ecb89b5ac665366aae9d50cf2ab9550051f3
Zeek <mzulkiful@yahoo.com>
2022-04-15 11:43:22 +0100

    Hamburger icon update and styling

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,19 @@
+"use strict";
 
+window.addEventListener("resize", ResizeWindow);
 
+function ResizeWindow() 
+{
+
+    var dataSpyList = [].slice.call(document.querySelectorAll('[data-spy = "scroll"]' ) );
+
+    dataSpyList.foreach (function (dataSpyElement)
+    {
+
+        bootstrap.Scrollpy.getInstance (dataSpyElement). refresh (
+
+        );
+    }
+    
+    );
+}
